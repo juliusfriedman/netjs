b75c713e0d5a1d798ab405baa479d0f4567ce6c3
Julius R Friedman <juliusfriedman@gmail.com>
2012-05-10 18:25:47 +0000

    Updated UnitTests and Casting Code

--- Cast.js
+++ Cast.js
@@ -3,14 +3,16 @@
     return Function.prototype.cast ? undefined : (function () {
 
         //Operator Overloading
+        function abstractError(constructor) { throw 'Cannot create an instance of an abstract class without a derived class! Type = ' + constructor; }
 
         //http://www.golimojo.com/etc/js-subclass.html
         //Modified for netjs by Julius Friedman
+        //Description: Helps interpreterd code to function correctly after compile with respect to instanceof
         function subclass(constructor, derivedConstructor) {
 
             //Possibly should augment derived to ensure it is not subclassed on accident again
 
-            if (constructor.abstract && !derivedConstructor) throw 'Cannot create an instance of an abstract class without a derived class! Type = ' + constructor;
+            if (constructor.abstract && !derivedConstructor) abstractError(constructor);
 
             function surrogateConstructor() { constructor.apply(derivedConstructor); }
 
@@ -25,6 +27,7 @@
         }
 
         //Make the concept of abstract
+        //Possibly should be true so functions cannot be called new on accident
         if (Object.definePropety) {
             Object.definePropety(Function.prototype, 'abstract', {
                 writable: true,
@@ -38,11 +41,15 @@
         } else Function.prototype.abstract = false;
 
         //Classes for testing
+        var baseClass = abstractError; //(this);
+        baseClass.abstract = true;
 
+        //Test class which can be instantiated derived from base
         var myClass = function () {
 
             //Privates
-            var intValue = Number(0),
+            var base = baseClass,
+                intValue = Number(0),
                 stringValue = String('');
 
             //Publics
@@ -51,15 +58,14 @@
                 return stringValue;
             }
 
-            this.cast = function (type, call) {
-                if (!type) return;
-                if (!call) return type.bind(this);
-                return call.bind(new type(this)).call(this);
-            }
+            this.cast = Function.prototype.cast;
 
         }
 
-        //Derived class
+        //Ensure instanceof works correctly
+        subclass(myClass, baseClass);
+
+        //Derived class from myClass
         var anotherClass = function () {
 
             //Store the base reference
@@ -84,10 +90,17 @@
             return call.bind(new type(this)).call(this);
         }
 
+        //Probably not needed
+        Function.prototype._call = Function.prototype.call;
+
+        //New call function intercept
+        Function.prototype.call = function () { return this.abstract ? abstractError(this.base || this.constructor || this.prototype || this) : Function.prototype._call.bind(this)(arguments) };
+
         //Ensure instanceof works correctly
         subclass(anotherClass, myClass);
 
         //Export Defined Classes for Unit Tests
+        window.baseClass = baseClass;
         window.myClass = myClass;
         window.anotherClass = anotherClass;
 
